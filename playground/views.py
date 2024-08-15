2ca8f9f3d8d661e1eab20acf8d7a5be954828988
Ajit <joinwithajith@gmail.com>
2024-08-15 10:38:07 +0530

    Initial commit

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Customer, Product
 
 
 # Create your views here.
 def hello(request):
-    x=1
-    y=2
-    
-    return render(request,"hello.html", {"name":"Ajith"})
+    customer = Customer.objects.all()
+    product = Product.objects.filter(pk=1).first()
+    membership = Customer.objects.filter(membership="G")
+    context = {"customer": customer, "product": product, "membership": membership}
+    return render(request, "hello.html", context)
--- store/models.py
+++ store/models.py
@@ -94,7 +94,10 @@ class Address(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=120)
     featured_product = models.ForeignKey(
-        Product, on_delete=models.SET_NULL, null=True, related_name="+"
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="+",
     )
 
 
