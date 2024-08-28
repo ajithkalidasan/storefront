f3872fb06b49769e5809b14b63a6a06896f9a330
Ajith K <ajithkalidasan@outlook.com>
2024-08-28 11:05:22 +0530

    Add sqlite as database

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "storefront",
-        "USER": "postgres",
-        "PASSWORD": "@jitH1998",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
