6a300cfddfae355635c9360eabd3baf8740db8a2
Ajith K <ajithkalidasan@outlook.com>
2024-08-29 11:51:02 +0530

    Added serializers

---
+++ store/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from decimal import Decimal
+from .models import  Product, Collection
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', ]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    price_with_tax = serializers.SerializerMethodField(method_name='calculated_tax')
+    collection = serializers.StringRelatedField()
+
+    class Meta:
+        model = Product
+        
+        fields = ['id', 'title', 'price', "price_with_tax", "collection"]
+        
+
+  
+
+    def calculated_tax(self, product: Product):
+        return product.price + product.price * Decimal(0.18)
